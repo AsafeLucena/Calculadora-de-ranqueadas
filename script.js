83026783f3a33b420552c9e97af4f5c2e2bbb368
Asafe Lucena <asafelucena07@outlook.com>
2023-12-23 11:03:29 -0300

    add script js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Calculadora de partidas ranqueadas")
+let saldo = vitorias(110,1)
+let eloHeroi = ""
+
+function vitorias (vitorias, derrotas){
+let saldoDeVitorias = vitorias - derrotas
+return saldoDeVitorias 
+}
+
+switch (true) {
+    case saldo < 10:
+       eloHeroi = "Ferro" 
+       break
+    case saldo >= 11 && saldo < 20:
+       eloHeroi = "Bronze" 
+       break
+    case saldo >= 21 && saldo < 50:
+       eloHeroi = "Prata" 
+       break 
+    case saldo >= 51 && saldo < 80:
+       eloHeroi = "Ouro" 
+       break 
+    case saldo >= 81 && saldo < 90:
+        eloHeroi = "Platina" 
+        break 
+    case saldo >= 91 && saldo < 100:
+        eloHeroi = "Diamante" 
+        break 
+    case saldo >= 101 && saldo < 110:
+        eloHeroi = "Mestre" 
+        break 
+    case saldo >= 110:
+        eloHeroi = "Desafiante" 
+        break     
+    }
+    
+    console.log(`O herói tem saldo de ${saldo} vitórias e seu elo é ${eloHeroi}`)
+
+function vitorias (vitorias, derrotas){
+let saldoDeVitorias = vitorias - derrotas
+return saldoDeVitorias 
+}
